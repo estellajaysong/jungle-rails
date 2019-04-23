905cf23f9d8dc31e91958506f8d92c4527e6d6f2
Estella Song <estellajaysong@gmail.com>
2019-04-23 20:49:06 +0000

    created feature spec for path from the home page to the product page, where it displays a single product's details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor clicks a product", type: :feature,  js: true do
+    #SETUP
+    before :each do
+      @category = Category.create! name: 'Apparel'
+  
+      10.times do |n|
+        @category.products.create!(
+          name:  Faker::Hipster.sentence(3),
+          description: Faker::Hipster.paragraph(4),
+          image: open_asset('apparel1.jpg'),
+          quantity: 10,
+          price: 64.99
+        )
+      end
+    end
+  
+    scenario "They see the product details" do
+      # ACT
+      visit root_path
+      first('article.product a').click
+  
+      # DEBUG 
+      sleep 3
+      save_screenshot
+
+      # VERIFY 
+      expect(page).to have_css '.product-detail', count: 1
+    end
+  
+end
